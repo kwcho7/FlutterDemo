0f784e1863c0e53406562469c5c48b26473c9f9a
chokyuwon <kwcho@mycreditchain.io>
2019-06-21 16:35:01 +0900

    added. layout builder example

--- lib/layout/layout_builder_page.dart
+++ lib/layout/layout_builder_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// Widget 을 Build 할때 부모의 크기를 미리 알 필요가 있을때 사용한다.
 class LayoutBuilderPage extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
@@ -10,6 +11,94 @@ class LayoutBuilderPage extends StatefulWidget{
 class LayoutBuilderState extends State<LayoutBuilderPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("LayoutBuilder");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("LayoutBuilder"),
+      ),
+      body: LayoutBuilder(
+        builder: (buildContext, constraint){
+          if(MediaQuery.of(buildContext).orientation == Orientation.landscape){
+            return LandscapeBodyWidget();
+          }
+          else {
+            return PortraitBodyWidget();
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+class LandscapeBodyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        ImageWidget(),
+        DescriptionWidget(),
+      ],
+    );
+  }
+}
+
+class PortraitBodyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ImageWidget(),
+        DescriptionWidget(),
+      ],
+    );
+  }
+}
+
+
+class ImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: Container(
+        color: Colors.blue[100],
+      ),
+    );
+  }
+}
+
+class DescriptionWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _buildCard("Card - 0"),
+            _buildCard("Card - 1"),
+            _buildCard("Card - 2"),
+            _buildCard("Card - 3"),
+            _buildCard("Card - 4"),
+            _buildCard("Card - 5"),
+            _buildCard("Card - 6"),
+            _buildCard("Card - 7"),
+            _buildCard("Card - 8"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(String text) {
+    return Card(
+      color: Colors.pink[200],
+      margin: EdgeInsets.all(4),
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.all(16),
+        child: Text(text),
+      ),
+    );
   }
 }
